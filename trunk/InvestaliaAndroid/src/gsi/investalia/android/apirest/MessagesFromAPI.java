1cf0967fcca31f9b72b58dcee944766aceffb82c
al.lopezf <al.lopezf@6691dbe0-0bb3-ee92-c97e-11a9c7b0af1c>
2010-10-03 15:04:27 +0000

    Fixed some bugs on the MessagesFromAPI

--- trunk/InvestaliaAndroid/src/gsi/investalia/android/apirest/MessagesFromAPI.java
+++ trunk/InvestaliaAndroid/src/gsi/investalia/android/apirest/MessagesFromAPI.java
@@ -6,8 +6,6 @@ import gsi.investalia.domain.Message;
 import gsi.investalia.domain.Tag;
 
 import java.io.BufferedReader;
-
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -18,6 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -65,7 +64,7 @@ public class MessagesFromAPI {
 	 * Authenticates using the Investalia API
 	 * @param username
 	 * @param password
-	 * @return true if the authentication is succesful.
+	 * @return true if the authentication is successful.
 	 */
 	public static boolean authenticateFromAPI(String username, String password){
 		if(connect(authenticate_TOKEN1+authenticate_TOKEN2+username+authenticate_TOKEN3+password).equals(authenticate_succesful))
@@ -76,11 +75,11 @@ public class MessagesFromAPI {
 	public static List<Message> getBlogsFromAPI (String lastUpdate){
 		/* Last Update es un número correspondiende con la última fecha que se actualizó */
 		/*Ej: 0000000000 devuelve todos los mensajes */
-		
-		ArrayList<Message> msgs = new ArrayList();
-		
-	       /* PARSEADO DEL XML , ya no es necesario.
-	        * try{
+
+		ArrayList<Message> msgs = new ArrayList<Message>();
+
+		/* PARSEADO DEL XML , ya no es necesario.
+		 * try{
 	            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 	            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 	            Document doc = docBuilder.parse (blogs_TOKEN1+blogs_TOKEN2+lastUpdate);//Conecta a través de la URI
@@ -93,9 +92,9 @@ public class MessagesFromAPI {
 	        }catch(Exception e){
 	        	System.err.println(e.getMessage());
 	        }*/
-		
+
 		try{
-			
+
 			JSONObject result;
 			String resultFromAPI = connect(blogs_TOKEN1+blogs_TOKEN2+lastUpdate);
 			result = new JSONObject(resultFromAPI);
@@ -106,25 +105,30 @@ public class MessagesFromAPI {
 				String userName = blogs.getJSONObject(i).getString("owner_name");
 				String title= blogs.getJSONObject(i).getString("title");
 				String text = blogs.getJSONObject(i).getString("description");
-				List<Tag> tags = new ArrayList();
-				tags.add(new Tag(0, "prueba")); // TODO: abajo está puesto el código para cogerlas, pero con los ejemplos no funciona
-				
-				/*String tags = blogs.getJSONObject(i).getString("tags");
-				JSONArray tags_j = blogs.getJSONObject(i).getJSONArray("tags");
-				List<Tag> tags = new ArrayList();
-				for(int j=0;j<tags_j.length();j++){
-					tags.add(new Tag(0, tags_j.getJSONObject(j).toString()));
-					System.out.println(tags.toString());
-				}*/
-				
-				
-				Date date = new Date();
-				//TODO: debería ser Date date = blogs.getJSONObject(i).getString("time_created); y algún tipo de parseado.
+				List<Tag> tags = new ArrayList<Tag>();
+				try{
+					JSONArray tags_j = blogs.getJSONObject(i).getJSONArray("tags");
+					for(int j=0;j<tags_j.length();j++){
+						tags.add(new Tag(0,tags_j.getString(j)));
+					}
+				}
+				catch(JSONException je){
+					tags.add(new Tag(0,blogs.getJSONObject(i).getString("tags")));
+				}
+
+				Date date = new Date(Long.parseLong(blogs.getJSONObject(i).getString("time_created"))*1000L);
 				boolean read =false;
 				boolean liked = false;
-				int rating= Integer.parseInt(blogs.getJSONObject(i).getString("rate")); //TODO: con uno de los blogs da error, no sé por qué. Con el resto va bien.
+				int rating;
+				
+				if(blogs.getJSONObject(i).getString("rate")!="null"){
+					rating= Integer.parseInt(blogs.getJSONObject(i).getString("rate"));
+				}else{
+					rating = 0;
+				}		
+				
 				int timesRead = 0; //Por defecto, 0
-				int idUserUpdating = 0; //TODO: qué es esto?
+				int idUserUpdating = 0; 
 				Message toBeAdded = new Message(id, userName, title, text,tags,date, read, liked,rating,timesRead,idUserUpdating);
 				msgs.add(toBeAdded);
 			}
